a4c9413310da0eef4105c5711fbe376e85b96378
weizhenye <weizhenye@gmail.com>
2016-10-12 01:57:47 +0800

    0.0.2

--- dist/vue-highcharts.js
+++ dist/vue-highcharts.js
@@ -2,35 +2,50 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('highcharts')) :
   typeof define === 'function' && define.amd ? define(['highcharts'], factory) :
   (global.VueHighcharts = factory(global.Highcharts));
-}(this, (function (Highcharts) { 'use strict';
+}(this, (function (HighchartsOnly) { 'use strict';
 
-Highcharts = 'default' in Highcharts ? Highcharts['default'] : Highcharts;
+HighchartsOnly = 'default' in HighchartsOnly ? HighchartsOnly['default'] : HighchartsOnly;
 
-var highcharts = {
-  name: 'highcharts',
-  template: '<div></div>',
-  props: {
-    options: Object
-  },
-  methods: {
-    render: function(options) {
-      var opts = options || {};
-      opts.chart = opts.chart || {};
-      opts.chart.renderTo = this.$el;
-      this._chart = new Highcharts.Chart(opts);
-    }
-  },
-  mounted: function() {
-    this.render(this.options);
-  },
-  beforeDestroy: function() {
-    this._chart.destroy();
+function create(tagName, Highcharts) {
+  var ctors = {
+    highcharts: 'Chart',
+    highstock: 'StockChart',
+    highmaps: 'Map'
+  };
+  var Ctor = Highcharts[ctors[tagName]];
+  if (!Ctor) {
+    return null;
   }
-};
+  return {
+    name: tagName,
+    template: '<div></div>',
+    props: {
+      options: Object
+    },
+    methods: {
+      render: function(options) {
+        var opts = options || {};
+        opts.chart = opts.chart || {};
+        opts.chart.renderTo = this.$el;
+        this._chart = new Ctor(opts);
+      }
+    },
+    mounted: function() {
+      this.render(this.options);
+    },
+    beforeDestroy: function() {
+      this._chart.destroy();
+    }
+  };
+}
 
-function install(Vue) {
+function install(Vue, options) {
+  var Highcharts = (options && options.Highcharts) || HighchartsOnly;
   Vue.prototype.Highcharts = Highcharts;
-  Vue.component('highcharts', highcharts);
+  ['highcharts', 'highstock', 'highmaps'].forEach(function(tagName) {
+    var component = create(tagName, Highcharts);
+    component && Vue.component(tagName, component);
+  });
 }
 
 return install;
