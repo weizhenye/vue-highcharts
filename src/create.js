3377a572bc5016e1ba6a5de8e18aa8a97a2248e4
weizhenye <weizhenye@gmail.com>
2016-10-14 01:07:56 +0800

    fix options with function

--- src/create.js
+++ src/create.js
@@ -20,7 +20,7 @@ function create(tagName, Highcharts) {
         if (isRenderer) {
           this.renderer = new Ctor(this.$el, this.width, this.height);
         } else {
-          var opts = JSON.parse(JSON.stringify(this.options));
+          var opts = this.options || {};
           opts.chart = opts.chart || {};
           opts.chart.renderTo = this.$el;
           this.chart = new Ctor(opts);
